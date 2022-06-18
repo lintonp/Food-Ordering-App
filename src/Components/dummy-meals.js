92f419976f8873c56b0a13aa1c5ca40d8b0152bc
lintonp <pereira.linton@gmail.com>
2022-06-18 16:21:49 +0530

    Updated Dummy Data

--- src/Components/dummy-meals.js
+++ src/Components/dummy-meals.js
@@ -1,27 +1,27 @@
 const DUMMY_MEALS = [
     {
       id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
+      name: 'Butter Chicken',
+      description: 'Pulled tandoori chicken in a classic makhani gravy finished with cream and kasoori methi',
+      price: 565,
     },
     {
       id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
+      name: 'Chicken Tikka Biryani',
+      description: 'Chicken tikka cooked with onion and mint layered with long grain basmati rice',
+      price: 575,
     },
     {
       id: 'm3',
       name: 'Barbecue Burger',
       description: 'American, raw, meaty',
-      price: 12.99,
+      price: 150,
     },
     {
       id: 'm4',
       name: 'Green Bowl',
       description: 'Healthy...and green...',
-      price: 18.99,
+      price: 200,
     },
   ];
 
